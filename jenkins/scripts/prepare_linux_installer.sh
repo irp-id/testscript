6b84e4e66d133f9ea6728913593d13bab3ad0910
ismail <ismail@klaviskripta.com>
2024-03-06 17:53:51 +0800

    Init commit

---
+++ jenkins/scripts/prepare_linux_installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x #echo on
+
+test -e build/result1 && mkdir -p build/ProductBuild/internal-artifact/$1/result &&
+cp -R build/result1/*.txt build/ProductBuild/internal-artifact/$1/result || echo skip result1 &&
+test -e build/result2/result2_1.txt && mkdir -p build/ProductBuild/internal-artifact &&
+cp -R build/result2/result2_1.txt build/ProductBuild/internal-artifact || echo skip result2
